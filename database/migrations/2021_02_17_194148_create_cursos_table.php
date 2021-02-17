09a5979fc38894b4d56c19a327e4d06ee8d62c80
Jhilario-dev <juan_l_hilario@hotmail.com>
2021-02-17 15:43:36 -0400

    borrado de tabla cursos y buelta a crear de nuevo

--- database/migrations/2021_02_17_194148_create_cursos_table.php
+++ database/migrations/2021_02_17_194148_create_cursos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Cursos extends Migration
+class CreateCursosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class Cursos extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->text('descripcion');
             $table->timestamps();
         });
