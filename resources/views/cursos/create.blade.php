62d552771eadb7177e594c31a8217bbb3257c1f7
Jhilario-dev <juan_l_hilario@hotmail.com>
2021-02-17 15:15:02 -0400

    primer commit

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.plantilla')
+
+@section('title', 'cursos create')
+
+
+
+@section('content')
+    <h1>en este curso podras crear cursos</h1>
+@endsection
---
+++ resources/views/cursos/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.plantilla')
+
+
+@section('title', "cursos $curso")
+
+
+@section('content')
+
+<h1>bienvenido al curso {{$curso}}</h1>
+@endsection
